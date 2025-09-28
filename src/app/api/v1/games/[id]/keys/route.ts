43ed3157165aafe5953d5b30d92c0b511b9310b1
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2025-09-28 21:57:50 +0200

    Implement key deletion route

--- src/app/api/v1/games/[id]/keys/route.ts
+++ src/app/api/v1/games/[id]/keys/route.ts
@@ -103,3 +103,74 @@ export async function POST(
 		);
 	}
 }
+
+export async function DELETE(
+	request: NextRequest,
+	{ params }: { params: Promise<{ id: string }> }
+) {
+	try {
+		await connectDB();
+
+		const body = await request.json();
+
+		if (!body || body.key == null) {
+			return NextResponse.json(
+				{
+					message: "Bad request",
+				},
+				{
+					status: 400,
+				}
+			);
+		}
+
+		const token = request.cookies.get("shibaCookie")?.value;
+		if (!token) {
+			return NextResponse.json(
+				{ error: "Unauthorized" },
+				{ status: 401 }
+			);
+		}
+
+		const payload = await getTokenPayload(token);
+		if (!payload) {
+			return NextResponse.json(
+				{ error: "Invalid token" },
+				{ status: 401 }
+			);
+		}
+
+		const game = await Game.findOne({
+			_id: (await params).id,
+			ownerSlackId: payload.slackId,
+		}).select("-__v");
+
+		if (!game) {
+			return NextResponse.json(
+				{ error: "Game not found" },
+				{ status: 404 }
+			);
+		}
+
+		const apiKeys = game.apiKeys;
+
+		if (apiKeys.includes(body.key)) {
+			const updatedKeys = apiKeys.filter(
+				(key: string) => key !== body.key
+			);
+			await game.updateOne({ apiKeys: updatedKeys });
+			return NextResponse.json({ message: "Success" }, { status: 200 });
+		} else {
+			return NextResponse.json(
+				{ message: "Key not found" },
+				{ status: 404 }
+			);
+		}
+	} catch (error) {
+		console.error("Error fetching game:", error);
+		return NextResponse.json(
+			{ error: "Internal server error" },
+			{ status: 500 }
+		);
+	}
+}
